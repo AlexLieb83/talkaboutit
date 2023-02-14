f6719e2420663852c4fa1d2480548fc3b9602244
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-02-14 16:33:20 -0600

    added text box for user

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import AddPost from "./components/AddPost";
+
 export default function Home() {
-  return <main className="">hi</main>;
+  return (
+    <main className="">
+      <h1>Hello Nextjs</h1>
+      <AddPost />
+    </main>
+  );
 }
